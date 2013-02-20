c3019d02fb2355d92d03a2ecaf6be13171e5bfbe
Ruben Fonseca <fonseka@gmail.com>
2013-02-20 20:00:21 +0000

    added client.get and docs

--- lib/invoicexpress/client/clients.rb
+++ lib/invoicexpress/client/clients.rb
@@ -4,25 +4,42 @@ module Invoicexpress
   class Client
     module Clients
 
-      # List clients
-      def list(options={})
+      # Returns all your clients
+      #
+      # @param options.per_page [Integer] You can specify how many results you want to fetch
+      # @param options.page [Integer] You can ask a specific page of clients
+      # @return [Array of Invoicexpress::Model::Client] An array with all your clients
+      def clients(options={})
         params = { :per_page => 30, :page => 1, :klass => Invoicexpress::Models::Client }
 
         get("clients.xml", params.merge(options))
       end
 
-      # Create a client
-      def create(client, options={})
+      # Creates a new client.
+      #
+      # @param client [Invoicexpress::Models::Client] The client to create
+      # @return [Invoicexpress::Models::Client] The new client from the server
+      def create_client(client, options={})
         if !client || !client.is_a?(Invoicexpress::Models::Client)
           raise ArgumentError, "Need a Invoicexpress::Models::Client instance"
         end
 
-        if client.name.empty?
+        if !client.name
           raise ArgumentError, "Client's name is required"
         end
 
         params = { :body => client, :klass => Invoicexpress::Models::Client }
         post("clients.xml", params.merge(options))
+      end
+
+      # Returns a specific client
+      #
+      # @param client_id [String] The client's ID
+      # @return [Invoicexpress::models::Client] The remote client from the server
+      def client(client_id, options={})
+        params = { :klass => Invoicexpress::Models::Client }
+
+        get("clients/#{client_id.to_s}.xml", params.merge(options))
       end
 
     end
--- spec/invoicexpress/client/clients_spec.rb
+++ spec/invoicexpress/client/clients_spec.rb
@@ -6,19 +6,19 @@ describe Invoicexpress::Client::Clients do
     @client = Invoicexpress::Client.new(:screen_name => "thinkorangeteste")
   end
 
-  describe ".list" do
+  describe ".clients" do
     it "returns all the clients" do
       stub_get("/clients.xml?per_page=30&page=1").
         to_return(xml_response("clients.list.xml"))
 
-      list = @client.list
+      list = @client.clients
       list.count.should == 1
       list.first.name.should == "Ruben Fonseca"
       list.first.email.should == "fonseka@gmail.com"
     end
   end
 
-  describe ".create" do
+  describe ".create_client" do
     it "creates a new client" do
       stub_post("/clients.xml").
         to_return(xml_response("clients.create.xml"))
@@ -28,9 +28,32 @@ describe Invoicexpress::Client::Clients do
         :email => "fonseka@gmail.com"
       })
 
-      new_client = @client.create(model_client)
+      new_client = @client.create_client(model_client)
       new_client.name.should == "Ruben Fonseca"
       new_client.email.should == "fonseka@gmail.com"
+    end
+
+    it "raises if no client is passed" do
+      expect {
+        @client.create_client(nil)
+      }.to raise_error(ArgumentError)
+    end
+
+    it "raises if the client has no name" do
+      expect {
+        @client.create_client(Invoicexpress::Models::Client.new())
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe ".get" do
+    it "gets a client" do
+      stub_get("/clients/1.xml").
+        to_return(xml_response("clients.get.xml"))
+
+      c = @client.client(1)
+      c.name.should == "Bruce Norris"
+      c.code.should == 1337
     end
   end
 
