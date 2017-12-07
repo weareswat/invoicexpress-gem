4ea4af6a89413ec8b7bc1ba2fc417d68bc70a7b2
Igor Escobar <igor.escobar@hole19golf.com>
2017-12-07 16:33:26 +0000

    adding missing attributes

--- lib/invoicexpress/models/sequence.rb
+++ lib/invoicexpress/models/sequence.rb
@@ -1,17 +1,35 @@
 module Invoicexpress
   module Models
-
     class Sequence < BaseModel
       include HappyMapper
 
       tag 'sequence'
       element :id, String
       element :serie, String
+      element :current_invoice_sequence_id, Integer
       element :current_invoice_number, Integer
+      element :current_credit_note_sequence_id, Integer
       element :current_credit_note_number, Integer
       element :current_debit_note_number, Integer
       element :default_sequence, Boolean
+      element :current_simplified_invoice_sequence_id, Integer
+      element :current_simplified_invoice_number, Integer
+      element :current_debit_note_sequence_id, Integer
+      element :current_debit_note_number, Integer
+      element :current_receipt_sequence_id, Integer
+      element :current_receipt_number, Integer
+      element :current_shipping_sequence_id, Integer
+      element :current_shipping_number, Integer
+      element :current_transport_sequence_id, Integer
+      element :current_transport_number, Integer
+      element :current_proforma_sequence_id, Integer
+      element :current_proforma_number, Integer
+      element :current_quote_sequence_id, Integer
+      element :current_quote_number, Integer
+      element :current_fees_note_sequence_id, Integer
+      element :current_fees_note_number, Integer
+      element :current_purchase_order_sequence_id, Integer
+      element :current_purchase_order_number, Integer
     end
-
   end
 end
